f289e390545a83319c5fd7a0705bc98df9e11e11
Rusty Thompson <rusted@rustedbrewer.com>
2022-08-05 08:08:47 -0500

    Implemented exclude filter.

---
+++ src/main/python/presets_exclude_filter.py
@@ -0,0 +1,22 @@
+import sys
+import json
+from abc import abstractmethod
+
+import yaml
+
+from presets_filter import PresetsFilter
+
+
+class PresetsExcludeFilter(PresetsFilter):
+
+    def __init__(self, presets_in, deep: bool):
+        super().__init__(presets_in, deep, presets_in)
+
+    def apply_filter_item(self, preset_id: str):
+        if preset_id in self.presets_out:
+            del self.presets_out[preset_id]
+        return self.presets_in[preset_id]
+
+    def init_filter(self):
+        pass
+
