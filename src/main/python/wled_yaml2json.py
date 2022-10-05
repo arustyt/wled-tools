def3e255d31c2346ef4b37e3348266d3a2d5b555
Rusty Thompson <rusted@rustedbrewer.com>
2022-10-05 07:25:28 -0500

    Backup output file if exists, don't add suffix if present in file name, added informational prints.

--- src/main/python/wled_yaml2json.py
+++ src/main/python/wled_yaml2json.py
@@ -1,6 +1,8 @@
 import argparse
+import os
 import sys
 import json
+from os.path import exists
 
 from presets_exclude_filter import PresetsExcludeFilter
 from presets_include_filter import PresetsIncludeFilter
@@ -104,8 +106,8 @@ def main(name, args):
     print("colors_path: {path}".format(path=colors_path))
 
     wled_presets = WledPresets(colors_path, pallets_path, effects_path)
+    print("Processing {file}".format(file=presets_path))
     preset_data = wled_presets.process_yaml_file(presets_path, segments_file=segments_path)
-    json_file_path = get_json_file_name(presets_path, suffix)
 
     if include_list is not None:
         include_filter = PresetsIncludeFilter(preset_data, deep)
@@ -114,12 +116,21 @@ def main(name, args):
         exclude_filter = PresetsExcludeFilter(preset_data, deep)
         preset_data = exclude_filter.apply(exclude_list)
 
+    json_file_path = get_json_file_name(presets_path, suffix)
+    if exists(json_file_path):
+        rename_existing_file(json_file_path)
+    print("Generating {file}".format(file=json_file_path))
     with open(json_file_path, "w") as out_file:
         json.dump(preset_data, out_file, indent=2)
 
+    print()
     wled_cfg = WledCfg(presets_data=preset_data)
+    print("Processing {file}".format(file=cfg_path))
     cfg_data = wled_cfg.process_yaml_file(cfg_path)
     json_file_path = get_json_file_name(cfg_path, suffix)
+    if exists(json_file_path):
+        rename_existing_file(json_file_path)
+    print("Generating {file}".format(file=json_file_path))
     with open(json_file_path, "w") as out_file:
         json.dump(cfg_data, out_file, indent=2)
 
@@ -129,8 +140,23 @@ def build_path(directory, file):
 
 
 def get_json_file_name(yaml_file_name: str, suffix: str):
-    json_file_name = yaml_file_name.replace('.yaml', suffix + '.json', 1)
+    file_base_name = yaml_file_name.replace('.yaml', '', 1)
+    if file_base_name.endswith(suffix):
+        json_file_name = '{base_name}.json'.format(base_name=file_base_name)
+    else:
+        json_file_name = '{base_name}{suffix}.json'.format(base_name=file_base_name, suffix=suffix)
+
     return json_file_name
+
+
+def rename_existing_file(file_path):
+    backup_file_path = "{file_path}.backup".format(file_path=file_path)
+    if exists(backup_file_path):
+        print("Removing existing backup file: {file}".format(file=backup_file_path))
+        os.remove(backup_file_path)
+
+    print("Renaming existing file from {file}\n                         to {backup_file}".format(file=file_path, backup_file=backup_file_path))
+    os.rename(file_path, backup_file_path)
 
 
 if __name__ == '__main__':
