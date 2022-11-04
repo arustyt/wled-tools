d03b5a5cbb23e1dcedbf4e6599237f14db241a85
Rusty Thompson <rusted@rustedbrewer.com>
2022-11-04 06:58:45 -0500

    Corrected content of upload body and added reboot after upload.  Also added reset which is not currently used.

--- src/main/python/wled_upload.py
+++ src/main/python/wled_upload.py
@@ -9,38 +9,92 @@ def main(name, args):
     parser.add_argument("--host", type=str, help="Hostname to which the file(s) will be uploaded.", action="store", required=True)
     parser.add_argument("--presets", type=str, help="Presets file to be uploaded.", action="store")
     parser.add_argument("--cfg", type=str, help="Cfg file to be uploaded.", action="store")
+    parser.add_argument('-v', '--verbose', action='store_true')
 
     args = parser.parse_args()
     host = str(args.host)
     presets_file = str(args.presets) if args.presets is not None else None
     cfg_file = str(args.cfg) if args.cfg is not None else None
-    print("host: " + host)
-    print("presets_file: " + str(presets_file))
-    print("cfg_file: " + str(cfg_file))
+    verbose = args.verbose
 
-    url = 'http://{host}/upload'.format(host=host)
-    # print("URL: " + url)
+    if verbose:
+        print("host: " + host)
+        print("presets_file: " + str(presets_file))
+        print("cfg_file: " + str(cfg_file))
+
+    base_url = 'http://{host}'.format(host=host)
+
+    file_uploaded = False
 
     if presets_file is not None:
-        upload_file(url, presets_file, 'presets.json')
+        upload_succeeded = upload_file(base_url, presets_file, '/presets.json')
+        if upload_succeeded:
+            file_uploaded = True
 
     if cfg_file is not None:
-        upload_file(url, cfg_file, 'cfg.json')
+        upload_succeeded = upload_file(base_url, cfg_file, '/cfg.json')
+        if upload_succeeded:
+            file_uploaded = True
+
+    if file_uploaded:
+        # reset_wled(base_url)
+        reboot_wled(base_url)
 
 
-def upload_file(url, src_file_name, dst_file_name):
-    presets_files = {'file': (dst_file_name, open(src_file_name, 'rb'))}
-    # print("presets_files: " + str(presets_files))
+def reboot_wled(base_url):
+    url = '{base_url}/settings/sec?'.format(base_url=base_url)
+    payload = 'OP=&AO=on&data=&data2='
+    cookies = {'Content-Type': 'application/x-www-form-urlencoded'}
+    print("Initiating reboot ... ", end='')
+
+    try:
+        reboot_response = requests.post(url, data=payload, cookies=cookies)
+        if reboot_response.ok:
+            print("successful.")
+            result = True
+        else:
+            print("failed.")
+            print(reboot_response.text)
+    except Exception as ex:
+        print("failed with exception.")
+        print(str(ex))
+
+
+def reset_wled(base_url):
+    url = '{base_url}/reset'.format(base_url=base_url)
+    print("Initiating reset ... ", end='')
+    try:
+        reset_response = requests.get(url)
+        if reset_response.ok:
+            print("successful.")
+            result = True
+        else:
+            print("failed.")
+            print(reset_response.text)
+    except Exception as ex:
+        print("failed with exception.")
+        print(str(ex))
+
+
+def upload_file(base_url, src_file_name, dst_file_name):
+    url = '{base_url}/upload'.format(base_url=base_url)
+    presets_files = {'file': (dst_file_name, open(src_file_name, 'rb'), 'application/json', {'name': 'data'})}
+
+    result = False
+    print("Uploading {file} ... ".format(file=src_file_name), end='')
     try:
         upload_response = requests.post(url, files=presets_files)
         if upload_response.ok:
-            print("{file} upload completed successfully!".format(file=src_file_name))
+            print("successful.")
+            result = True
         else:
-            print("{file} upload failed.".format(file=src_file_name))
+            print("failed.")
             print(upload_response.text)
     except Exception as ex:
-        print("{file} upload failed.".format(file=src_file_name))
+        print("failed with exception.")
         print(str(ex))
+
+    return result
 
 
 if __name__ == '__main__':
