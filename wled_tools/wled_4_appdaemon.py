565feaa9bfdc3f0c3e8e056c833abc6026f84a0a
Rusty Thompson <rusted@rustedbrewer.com>
2023-12-14 09:57:35 -0600

    Added support for testing.

--- wled_tools/wled_4_appdaemon.py
+++ wled_tools/wled_4_appdaemon.py
@@ -8,6 +8,8 @@ ENV = "env"
 JOB = "job"
 DATE_STR = "date_str"
 VERBOSE = "verbose"
+TEST_START = "test_start"
+TEST_INTERVAL = "test_interval"
 
 
 # Declare Class
@@ -25,6 +27,16 @@ class WledLightsLoader(hass.Hass):
         else:
             self.date_str = None
 
+        if TEST_START in self.args:
+            self.test_start = self.args[TEST_START]
+        else:
+            self.test_start = None
+
+        if TEST_INTERVAL in self.args:
+            self.test_interval = self.args[TEST_INTERVAL]
+        else:
+            self.test_interval = None
+
         if VERBOSE in self.args:
             self.verbose = self.args[VERBOSE]
         else:
@@ -34,13 +46,16 @@ class WledLightsLoader(hass.Hass):
         self.env = self.args[ENV]
 
     def initialize(self):
-        run_time_parts = self.run_time.split(':')
-        run_hour = int(run_time_parts[0])
-        run_min = int(run_time_parts[1])
-        run_sec = int(run_time_parts[2])
 
-        time = datetime.time(run_hour, run_min, run_sec)
-        self.run_daily(self.install_lights_de_jour, time)
+        if self.test_start is None or self.test_interval is None:
+            run_time_parts = self.run_time.split(':')
+            run_hour = int(run_time_parts[0])
+            run_min = int(run_time_parts[1])
+            run_sec = int(run_time_parts[2])
+            time = datetime.time(run_hour, run_min, run_sec)
+            self.run_daily(self.install_lights_de_jour, time)
+        else:
+            self.run_every(self.install_lights_de_jour, self.test_start, int(self.test_interval))
 
     def install_lights_de_jour(self, cb_args):
         process_successful = wled_4_ha(job_file=self.job, env=self.env, date_str=self.date_str, verbose=self.verbose)
