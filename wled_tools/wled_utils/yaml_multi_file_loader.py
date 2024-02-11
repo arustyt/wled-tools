04a9e99277fa63f781eb009220e716187dbbd473
Rusty Thompson <rusted@rustedbrewer.com>
2024-02-10 19:36:22 -0600

    Added ability to merge list data when multiple yaml files are loaded.

--- wled_tools/wled_utils/yaml_multi_file_loader.py
+++ wled_tools/wled_utils/yaml_multi_file_loader.py
@@ -1,11 +1,11 @@
 import yaml
 
 
-def load_yaml_files(yaml_file_list):
+def load_yaml_files(yaml_file_list, merge_lists=True):
     yaml_data = {}
 
     for yaml_file in yaml_file_list:
-        yaml_data = add_yaml_file(yaml_file, yaml_data)
+        yaml_data = add_yaml_file(yaml_file, yaml_data, merge_lists)
 
     return yaml_data
 
@@ -16,13 +16,14 @@ def load_yaml_file(yaml_file):
     return yaml_data
 
 
-def add_yaml_file(yaml_file, yaml_data):
+def add_yaml_file(yaml_file, yaml_data, merge_lists=True):
     with open(yaml_file) as in_file:
         new_yaml_data: dict = yaml.safe_load(in_file)
-
         for key in new_yaml_data.keys():
             if key not in yaml_data:
                 yaml_data[key] = new_yaml_data[key]
+            elif merge_lists and isinstance(yaml_data[key], list):
+                yaml_data[key].extend(new_yaml_data[key])
             else:
                 raise ValueError('Duplicate keys in yaml file: file = {file}, key = {key}'.format(file=yaml_file, key=key))
 
