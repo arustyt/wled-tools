694f66f6fb6d898c50e53eeec34701cc38157d47
Rusty Thompson <rusted@rustedbrewer.com>
2024-11-01 09:34:49 -0500

    Corrected variable name

--- wled_tools/wled_utils/rrule_utils.py
+++ wled_tools/wled_utils/rrule_utils.py
@@ -11,23 +11,23 @@ def get_frequency(frequency_string: str):
 def get_byweekday(dow, occurrence):
     day_of_week_abbreviation = dow.upper()[0:2]
     if day_of_week_abbreviation == 'MO':
-        weeday_obj = MO(occurrence)
+        weekday_obj = MO(occurrence)
     elif day_of_week_abbreviation == 'TU':
-        weeday_obj = TU(occurrence)
+        weekday_obj = TU(occurrence)
     elif day_of_week_abbreviation == 'WE':
-        weeday_obj = WE(occurrence)
+        weekday_obj = WE(occurrence)
     elif day_of_week_abbreviation == 'TH':
-        weeday_obj = TH(occurrence)
+        weekday_obj = TH(occurrence)
     elif day_of_week_abbreviation == 'FR':
-        weeday_obj = FR(occurrence)
+        weekday_obj = FR(occurrence)
     elif day_of_week_abbreviation == 'SA':
-        weeday_obj = SA(occurrence)
+        weekday_obj = SA(occurrence)
     elif day_of_week_abbreviation == 'SU':
-        weeday_obj = SU(occurrence)
+        weekday_obj = SU(occurrence)
     else:
         raise ValueError("Invalid day of week.")
 
-    return weeday_obj
+    return weekday_obj
 
 
 def interpret_general_rrule(frequency, by_month, by_weekday, first_day_of_year):
