919ce2ff93274f23a25467976964daf9263c05a7
Rusty Thompson <rusted@rustedbrewer.com>
2025-01-21 08:57:54 -0600

    Renamed class variables to remove name conflict.

--- wled_tools/wled_4_appdaemon.py
+++ wled_tools/wled_4_appdaemon.py
@@ -32,9 +32,9 @@ class Wled4Appdaemon(hass.Hass):
         self.job = self.get_required_arg_value(JOB_ARG)
         self.env = self.get_required_arg_value(ENV_ARG)
 
-        self.run_in = self.get_optional_arg_value(RUN_IN_ARG, None)
-        self.run_every = self.get_optional_arg_value(RUN_EVERY_ARG, None)
-        self.run_daily = self.get_optional_arg_value(RUN_DAILY_ARG, None)
+        self.run_in_cfg = self.get_optional_arg_value(RUN_IN_ARG, None)
+        self.run_every_cfg = self.get_optional_arg_value(RUN_EVERY_ARG, None)
+        self.run_daily_cfg = self.get_optional_arg_value(RUN_DAILY_ARG, None)
         self.verbose = self.get_optional_arg_value(VERBOSE_ARG, False)
         self.config_repo = self.get_optional_arg_value(CONFIG_REPO_ARG, None)
         self.config_remote = self.get_optional_arg_value(CONFIG_REMOTE_ARG, None)
@@ -64,14 +64,14 @@ class Wled4Appdaemon(hass.Hass):
         return arg_value
 
     def initialize(self):
-        if self.run_in is not None:
-            self.init_run_in_config(self.run_in)
+        if self.run_in_cfg is not None:
+            self.init_run_in_config(self.run_in_cfg)
 
-        if self.run_every is not None:
-            self.init_run_every_config(self.run_every)
+        if self.run_every_cfg is not None:
+            self.init_run_every_config(self.run_every_cfg)
 
-        if self.run_daily is not None:
-            self.init_run_daily_config(self.run_daily)
+        if self.run_daily_cfg is not None:
+            self.init_run_daily_config(self.run_daily_cfg)
 
     def init_run_in_config(self, run_in_args):
         if run_in_args is None:
@@ -86,7 +86,7 @@ class Wled4Appdaemon(hass.Hass):
 
     def init_run_in(self, run_in_delay):
         self.log_info('Initializing run_in @ {} seconds.'.format(run_in_delay))
-        self.run_in(self.execute_task, int(run_in_delay))
+        self.run_in(self.callback, int(run_in_delay))
 
     def init_run_every_config(self, run_every_args):
         if run_every_args is None:
@@ -109,7 +109,7 @@ class Wled4Appdaemon(hass.Hass):
             interval = run_every_arg
 
         self.log_info('Initializing run_every @ {}, every {} seconds.'.format(start, interval))
-        self.run_every(self.execute_task, start, int(interval))
+        self.run_every(self.callback, start, int(interval))
 
     def init_run_daily_config(self, run_daily_args):
         if run_daily_args is None:
@@ -139,7 +139,7 @@ class Wled4Appdaemon(hass.Hass):
         run_sec = int(groups[2])
         self.log_info("Initializing daily mode @ {}:{}:{}".format(run_hour, run_min, run_sec))
         time = datetime.time(run_hour, run_min, run_sec)
-        self.run_daily(self.execute_task, time)
+        self.run_daily(self.callback, time)
 
     def init_sun_mode(self, groups):
         sun_event = groups[0]
@@ -159,13 +159,13 @@ class Wled4Appdaemon(hass.Hass):
 
     def init_sunset_mode(self, offset):
         self.log_info("Initializing sunset mode with offset: {offset}".format(offset=offset))
-        self.run_at_sunset(self.execute_task, offset=offset)
+        self.run_at_sunset(self.callback, offset=offset)
 
     def init_sunrise_mode(self, offset):
         self.log_info("Initializing sunrise mode with offset: {offset}".format(offset=offset))
-        self.run_at_sunrise(self.execute_task, offset=offset)
+        self.run_at_sunrise(self.callback, offset=offset)
 
-    def execute_task(self, cb_args):
+    def callback(self, cb_args):
         self.install_presets_de_jour()
 
     def install_presets_de_jour(self):
