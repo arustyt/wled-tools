b3e72a09f204a5556ea729d237e2445ec6100e7d
Rusty Thompson <rusted@rustedbrewer.com>
2025-01-23 14:18:19 -0600

    Attempt to get MQTT working without inheriting from mqttapi.Mqtt.

--- wled_tools/mqtt_4_appdaemon.py
+++ wled_tools/mqtt_4_appdaemon.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 
 import mqttapi as mqtt
+import hassapi as hass
 
 from appdaemon_tools.appdaemon_actions import send_current_holiday_to_ha, install_presets_de_jour
 from appdaemon_tools.helper_4_appdaemon import Helper4Appdaemon
@@ -20,21 +21,23 @@ APPDAEMON_ACTION_TAG = 'action'
 #  'action': 'send_holiday' | 'install_presets'}
 
 
-class Mqtt4Appdaemon(mqtt.Mqtt):
+class Mqtt4Appdaemon(hass.Hass):
 
     def __init__(self, *args):
         super().__init__(*args)
-
+        self.mqtt = None
         self.helper = Helper4Appdaemon(args)
         self.cmd_topic = self.helper.get_required_arg_value(APPDAEMON_CMD_TOPIC_TAG)
         self.namespace = self.helper.get_required_arg_value(APPDAEMON_NAMESPACE_TAG)
 
     @abstractmethod
     def initialize(self):
-        self.set_namespace(self.namespace)
-        self.mqtt_subscribe(self.cmd_topic, namespace=self.namespace)
+        self.mqtt = self.get_plugin_api("MQTT")
+
+        self.mqtt.set_namespace(self.namespace)
+        self.mqtt.mqtt_subscribe(self.cmd_topic, namespace=self.namespace)
         # self.call_service("mqtt/subscribe", topic=self.cmd_topic, namespace=self.namespace)
-        self.listen_event(self.process_mqtt_event, namespace=self.namespace)
+        self.mqtt.listen_event(self.process_mqtt_event, namespace=self.namespace)
 
     def process_mqtt_event(self, event_name, data, cb_args):
         self.helper.log_info("GOT EVENT: event_name: {}, data: {}".format(event_name, data))
