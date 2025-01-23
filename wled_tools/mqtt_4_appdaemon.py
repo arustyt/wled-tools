80e439e10188593e7d415db18143024529eb6197
Rusty Thompson <rusted@rustedbrewer.com>
2025-01-23 17:47:54 -0600

    Back to inheriting from  mqttapi.Mqtt.

--- wled_tools/mqtt_4_appdaemon.py
+++ wled_tools/mqtt_4_appdaemon.py
@@ -21,7 +21,7 @@ APPDAEMON_ACTION_TAG = 'action'
 #  'action': 'send_holiday' | 'install_presets'}
 
 
-class Mqtt4Appdaemon(hass.Hass):
+class Mqtt4Appdaemon(mqtt.Mqtt):
 
     def __init__(self, *args):
         super().__init__(*args)
@@ -32,11 +32,16 @@ class Mqtt4Appdaemon(hass.Hass):
 
     @abstractmethod
     def initialize(self):
-        self.mqtt = self.get_plugin_api("MQTT")
+        # self.mqtt = self.get_plugin_api("MQTT")
 
         self.mqtt.set_namespace(self.namespace)
-        self.mqtt.mqtt_subscribe(self.cmd_topic, namespace=self.namespace)
-        # self.call_service("mqtt/subscribe", topic=self.cmd_topic, namespace=self.namespace)
+        try:
+            self.helper.log_info("Trying self.mqtt.mqtt_subscribe({}, namespace={})".format(self.cmd_topic, self.namespace))
+            self.mqtt.mqtt_subscribe(self.cmd_topic, namespace=self.namespace)
+        except Exception as ex:
+            self.helper.log_info("Trying self.call_service({}, topic={}, namespace={})".format("mqtt/subscribe", self.cmd_topic, self.namespace))
+            self.call_service("mqtt/subscribe", topic=self.cmd_topic, namespace=self.namespace)
+            raise ex
         self.mqtt.listen_event(self.process_mqtt_event, namespace=self.namespace)
 
     def process_mqtt_event(self, event_name, data, cb_args):
