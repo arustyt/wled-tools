785bfab078394b674983129ad846750af5970b9c
Rusty Thompson <rusted@rustedbrewer.com>
2025-01-24 10:11:14 -0600

    Reverted back to inheriting from hass.Hass with implementations with and without namespace.

--- wled_tools/mqtt_4_appdaemon.py
+++ wled_tools/mqtt_4_appdaemon.py
@@ -21,30 +21,64 @@ APPDAEMON_ACTION_TAG = 'action'
 #  'action': 'send_holiday' | 'install_presets'}
 
 
-class Mqtt4Appdaemon(mqtt.Mqtt):
+class Mqtt4Appdaemon(hass.Hass):
 
     def __init__(self, *args):
         super().__init__(*args)
-        # self.mqtt = None
+        self.mqtt = None
         self.helper = Helper4Appdaemon(args)
         self.cmd_topic = self.helper.get_required_arg_value(APPDAEMON_CMD_TOPIC_TAG)
-        self.namespace = self.helper.get_required_arg_value(APPDAEMON_NAMESPACE_TAG)
+        self.namespace = self.helper.get_optional_arg_value(APPDAEMON_NAMESPACE_TAG, None)
 
     @abstractmethod
     def initialize(self):
+        self.mqtt = self.get_plugin_api("MQTT")
+        if self.namespace is not None:
+            self.try_with_namespace()
+        else:
+            self.try_without_namespace()
+
+    def try_without_namespace(self):
+        try:
+            self.helper.log_info(
+                "Trying self.mqtt.mqtt_subscribe({})".format(self.cmd_topic))
+            self.mqtt_subscribe(self.cmd_topic)
+            self.helper.log_info(
+                "Trying self.mqtt.mqtt_subscribe({})".format(self.cmd_topic))
+            self.mqtt_subscribe(self.cmd_topic)
+        except Exception as ex:
+            self.helper.log_error(
+                "Trying self.mqtt.mqtt_subscribe({}) - FAILED".format(self.cmd_topic))
+        try:
+            self.helper.log_info(
+                "Trying self.call_service({}, topic={})".format("mqtt/subscribe", self.cmd_topic))
+            self.call_service("mqtt/subscribe", topic=self.cmd_topic)
+        except Exception as ex:
+            self.helper.log_error(
+                "Trying self.call_service({}, topic={}) - FAILED".format("mqtt/subscribe", self.cmd_topic))
+        self.listen_event(self.process_mqtt_event)
+
+    def try_with_namespace(self):
         self.set_namespace(self.namespace)
         try:
-            self.helper.log_info("Trying self.mqtt.mqtt_subscribe({}, namespace={})".format(self.cmd_topic, self.namespace))
+            self.helper.log_info(
+                "Trying self.mqtt.mqtt_subscribe({}, namespace={})".format(self.cmd_topic, self.namespace))
+            self.mqtt_subscribe(self.cmd_topic, namespace=self.namespace)
+            self.helper.log_info(
+                "Trying self.mqtt.mqtt_subscribe({}, namespace={})".format(self.cmd_topic, self.namespace))
             self.mqtt_subscribe(self.cmd_topic, namespace=self.namespace)
         except Exception as ex:
-            self.helper.log_error("Trying self.mqtt.mqtt_subscribe({}, namespace={} - FAILED)".format(self.cmd_topic, self.namespace))
-
+            self.helper.log_error(
+                "Trying self.mqtt.mqtt_subscribe({}, namespace={} - FAILED)".format(self.cmd_topic, self.namespace))
         try:
-            self.helper.log_info("Trying self.call_service({}, topic={}, namespace={})".format("mqtt/subscribe", self.cmd_topic, self.namespace))
+            self.helper.log_info(
+                "Trying self.call_service({}, topic={}, namespace={})".format("mqtt/subscribe", self.cmd_topic,
+                                                                              self.namespace))
             self.call_service("mqtt/subscribe", topic=self.cmd_topic, namespace=self.namespace)
         except Exception as ex:
-            self.helper.log_error("Trying self.call_service({}, topic={}, namespace={} - FAILED)".format("mqtt/subscribe", self.cmd_topic, self.namespace))
-
+            self.helper.log_error(
+                "Trying self.call_service({}, topic={}, namespace={} - FAILED)".format("mqtt/subscribe", self.cmd_topic,
+                                                                                       self.namespace))
         self.listen_event(self.process_mqtt_event, namespace=self.namespace)
 
     def process_mqtt_event(self, event_name, data, cb_args):
