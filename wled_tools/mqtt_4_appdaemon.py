ddcf815e16189b958ab1e4b5403b2e123bde7775
Rusty Thompson <rusted@rustedbrewer.com>
2025-01-24 14:15:08 -0600

    Implemented action calls for incoming MQTT messages.

--- wled_tools/mqtt_4_appdaemon.py
+++ wled_tools/mqtt_4_appdaemon.py
@@ -4,7 +4,8 @@ import mqttapi as mqtt
 import hassapi as hass
 
 from appdaemon_tools.appdaemon_actions import send_current_holiday_to_ha, install_presets_de_jour
-from appdaemon_tools.helper_4_appdaemon import Helper4Appdaemon
+from appdaemon_tools.ha_4_appdaemon import JOB_ARG, VERBOSE_ARG
+from appdaemon_tools.helper_4_appdaemon import Helper4Appdaemon, ENV_ARG
 
 DEFAULT_APPDAEMON_CMD_TOPIC = 'appdaemon/cmd'
 APPDAEMON_CMD_TOPIC_TAG = "cmd_topic"
@@ -36,4 +37,19 @@ class Mqtt4Appdaemon(hass.Hass):
         self.mqtt.listen_event(self.process_mqtt_event, namespace=self.namespace)
 
     def process_mqtt_event(self, event_name, data, cb_args):
-        self.helper.log_info("GOT EVENT: event_name: {}, data: {}".format(event_name, data))
+        if event_name == 'MQTT_MESSAGE':
+            app = data['payload'][APPDAEMON_APP_TAG]
+            action = data['payload'][APPDAEMON_ACTION_TAG]
+            app_data = self.helper.app_config[app]
+            job = app_data[JOB_ARG]
+            env = app_data[ENV_ARG]
+            verbose = self.helper.get_optional_arg_value(VERBOSE_ARG, False)
+
+            if action == 'send_holiday':
+                send_current_holiday_to_ha(job=job, env=env, verbose=verbose, helper=self.helper, mqttapi=self.mqtt)
+                return
+            elif action == 'install_presets':
+                install_presets_de_jour(job=job, env=env, verbose=verbose, helper=self.helper)
+                return
+
+        self.helper.log_info("GOT UNHANDLED EVENT: event_name: {}, data: {}".format(event_name, data))
