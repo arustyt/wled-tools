3a93f74d6dc6223ffada52457697ace4049a8ed2
Rusty Thompson <rusted@rustedbrewer.com>
2025-01-24 15:06:44 -0600

    Added run_hourly support.

--- wled_tools/appdaemon_tools/ha_4_appdaemon.py
+++ wled_tools/appdaemon_tools/ha_4_appdaemon.py
@@ -9,6 +9,7 @@ from helper_4_appdaemon import Helper4Appdaemon
 
 JOB_ARG = "job"
 RUN_DAILY_ARG = 'run_daily'
+RUN_HOURLY_ARG = 'run_hourly'
 RUN_EVERY_ARG = 'run_every'
 RUN_IN_ARG = 'run_in'
 DATE_STR_ARG = "date_str"
@@ -33,6 +34,7 @@ class Ha4Appdaemon(hass.Hass):
         self.run_in_cfg = self.helper.get_optional_arg_value(RUN_IN_ARG, None)
         self.run_every_cfg = self.helper.get_optional_arg_value(RUN_EVERY_ARG, None)
         self.run_daily_cfg = self.helper.get_optional_arg_value(RUN_DAILY_ARG, None)
+        self.run_hourly_cfg = self.helper.get_optional_arg_value(RUN_HOURLY_ARG, None)
         self.date_str = self.helper.get_optional_arg_value(DATE_STR_ARG, None)
         self.verbose = self.helper.get_optional_arg_value(VERBOSE_ARG, False)
         self.config_repo = self.helper.get_optional_arg_value(CONFIG_REPO_ARG, None)
@@ -56,6 +58,9 @@ class Ha4Appdaemon(hass.Hass):
 
         if self.run_daily_cfg is not None:
             self.init_run_daily_config(self.run_daily_cfg)
+
+        if self.run_hourly_cfg is not None:
+            self.init_run_hourly_config(self.run_hourly_cfg)
 
     def init_run_in_config(self, run_in_args):
         if run_in_args is None:
@@ -96,6 +101,29 @@ class Ha4Appdaemon(hass.Hass):
 
         self.helper.log_info('Initializing run_every @ {}, every {} seconds.'.format(start, interval))
         self.run_every(self.callback, start, int(interval))
+
+    def init_run_hourly_config(self, run_hourly_args):
+        if run_hourly_args is None:
+            return
+        if isinstance(run_hourly_args, str):
+            self.init_run_every(run_hourly_args)
+        elif isinstance(run_hourly_args, list):
+            for run_every_arg in run_hourly_args:
+                self.init_run_every(run_every_arg)
+        else:
+            raise ValueError("Unsupported run-every value, {}".format(run_hourly_args))
+
+    def init_run_hourly(self, run_hourly_arg):
+        if ',' in run_hourly_arg:
+            run_times = run_hourly_arg.split(',')
+        else:
+            run_times = [run_hourly_arg]
+
+        for run_time in run_times:
+            self.helper.log_info('Initializing run_hourly @ {} minutes past the hour.'.format(run_time))
+            hms = run_time.split(':')
+            runtime = datetime.time(0, hms[1], 0)
+            self.run_hourly(self.callback, runtime)
 
     def init_run_daily_config(self, run_daily_args):
         if run_daily_args is None:
